683a1dde8a3838cea3ebf384949ae1fcf3dd93a8
HarlamovEvgeniy <harlamov.mail@mail.ru>
2023-10-10 18:36:04 +0300

    Feature: Favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link rel="icon" href="/b2b/favicon.ico" sizes="any" />
         <link
           rel='preconnect'
           href='https://fonts.googleapis.com'
